3a2e1d772c6cd7b99fc6050a71055eaa57f57489
unknown <yummyfunnybunny13@gmail.com>
2021-01-28 10:56:00 -0500

    added comments and refactored code

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,3 +1,4 @@
+// ANCHOR -- Element Selectors --
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
@@ -5,7 +6,14 @@ const circles = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
-next.addEventListener("click", () => {
+// ANCHOR -- Event Listeners --
+next.addEventListener("click", increaseActive);
+prev.addEventListener("click", decreaseActive);
+
+// SECTION -- Functions --
+
+// ANCHOR -- Increase Actives --
+function increaseActive() {
   // increase currentActive
   currentActive++;
 
@@ -13,34 +21,45 @@ next.addEventListener("click", () => {
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
-  update();
-});
 
-prev.addEventListener("click", () => {
-  // increase currentActive
+  // update the progress bar
+  updateProgressBar();
+}
+
+// ANCHOR -- Decrease Actives --
+function decreaseActive() {
+  // decrease currentActive
   currentActive--;
 
-  // Prevent currentActive from exceeding the max number of steps
+  // Prevent currentActive from exceeding the minimum allowed
   if (currentActive < 1) {
     currentActive = 1;
   }
 
-  update();
-});
+  // update the progress bar
+  updateProgressBar();
+}
 
-function update() {
+// ANCHOR -- Update Progress Bar --
+function updateProgressBar() {
+  // cycle through the circles node list
   circles.forEach((circle, index) => {
+    // if the index is less than currentActive...
     if (index < currentActive) {
+      // add the active class
       circle.classList.add("active");
     } else {
+      // otherwise, remove the active class
       circle.classList.remove("active");
     }
   });
 
+  // set the apropriate length of the progress bar based on the active circles
   const actives = document.querySelectorAll(".active");
   progress.style.width =
     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
 
+  // set the buttons to disabled depending on currentActive
   if (currentActive === 1) {
     prev.disabled = true;
   } else if (currentActive === circles.length) {
