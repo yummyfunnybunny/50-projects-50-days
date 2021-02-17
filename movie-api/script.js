18b2d4d98fa62b9dc93a07a6e31fbb7780e6edf5
unknown <yummyfunnybunny13@gmail.com>
2021-02-17 09:14:18 -0500

    added drawing app project

--- movie-api/script.js
+++ movie-api/script.js
@@ -1,3 +1,8 @@
+// -------------------------------
+// NOTE REMOVE BEFORE EVERY GITHUB PUSH
+
+// REMOVE BEFORE EVERY GITHUB PUSH
+// -------------------------------
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
 const form = document.getElementById("form");
